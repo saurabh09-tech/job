493c635f5acc16a8efb240598cbeff709e402f6b
SAURABH <saurabh@192.168.1.35>
2025-03-26 16:40:57 +0545

    key

--- src/pages/Tech.jsx
+++ src/pages/Tech.jsx
@@ -17,7 +17,7 @@ function Tech() {
   return (
     <div className='grid grid-cols-5 justify-around gap-[30px] bg-gray-200 p-[30px]'>
       {data.map((e)=>{
-        return   <div className='p-[30px] aspect-square bg-white flex flex-col justify-center items-center gap-[20px]'>
+        return   <div key={e.title}className='p-[30px] aspect-square bg-white flex flex-col justify-center items-center gap-[20px]'>
         <div className='h-[40px] aspect-square rounded-full bg-emerald-200 flex justify-center items-center'> {e.icon}</div>
          <p>{e.title}</p>
 
